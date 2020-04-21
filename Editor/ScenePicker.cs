5d8710816844bd222cbd533f3d3f141c7ec67825
Marko Francuski <francmarko@yahoo.com>
2020-04-21 11:53:34 +0200

    Add scene picker

---
+++ Editor/ScenePicker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class ScenePicker : EditorWindow
+{
+
+    [MenuItem("Window/Custom Windows/Scene Picker")]
+    public static void ShowWindow()
+    {
+        GetWindow<ScenePicker>("Scene Picker");
+    }
+
+    private void OnGUI()
+    {
+        if(GUILayout.Button("Name your Scene"))
+        {
+            //Load Splash Scene
+            EditorSceneManager.OpenScene("PathToYourScene.unity");
+        }
+
+
+
+    }
+}
