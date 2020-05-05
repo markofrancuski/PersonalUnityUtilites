9d7d3b7cacf357451ed10acb55ee22e8fc2edd9f
Marko Francuski <francmarko@yahoo.com>
2020-05-05 15:33:28 +0200

    Add Prefab Opener

---
+++ Editor/PrefabOpener.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class PrefabOpener : EditorWindow
+{
+    [MenuItem("Window/Custom Windows/Prefab Opener")]
+    public static void ShowWindow()
+    {
+        GetWindow<PrefabOpener>("Prefab Opener");
+    }
+
+    private void OnGUI()
+    { 
+        if (GUILayout.Button("Open prefab"))
+        {
+            // GameObject obj = Selection.activeObject as GameObject;
+            Object obj = AssetDatabase.LoadAssetAtPath<Object>("Assets/Resources/PlatformChunks/Level 1.prefab");
+            AssetDatabase.OpenAsset(obj);
+        }
+    }
+}
