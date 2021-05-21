82004412facb688d8dfe034ecdc54a2c1fd989d9
Marko Francuski <39244428+markofrancuski@users.noreply.github.com>
2021-05-22 01:08:25 +0200

    Add Custom Menu Item Tools Script
    You can open file explorer to the application persistence path

---
+++ Editor/CustomMenuItemTools.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class CustomMenuItemTools
+{
+    [MenuItem("Custom Tools/Open Persistence Folder")]
+    static void ApplicationPersistence()
+    {
+        System.Diagnostics.Process.Start("explorer.exe", Application.persistentDataPath.Replace('/', '\\'));
+    }
+}
