84387d607d36ccb760261ad5b6c27ad4439564ca
Marko Francuski <39244428+markofrancuski@users.noreply.github.com>
2021-08-08 20:37:09 +0200

    Add Root project + deleting whole persistent directory.

--- Editor/CustomMenuItemTools.cs
+++ Editor/CustomMenuItemTools.cs
@@ -1,11 +1,31 @@
-﻿using UnityEditor;
 using UnityEngine;
+using UnityEditor;
+using System.IO;
 
 public class CustomMenuItemTools
 {
-    [MenuItem("Custom Tools/Open Persistence Folder")]
-    static void ApplicationPersistence()
+    [MenuItem("Custom Tools/Delete Persistence", priority = 0)]
+    static void DeletPresistance()
+    {
+        Directory.Delete(Application.persistentDataPath, true);
+    }
+
+    [MenuItem("Custom Tools/Persistence Folder")]
+    static void PersistenceProjectFolder()
     {
         System.Diagnostics.Process.Start("explorer.exe", Application.persistentDataPath.Replace('/', '\\'));
     }
+
+    [MenuItem("Custom Tools/Root Folder")]
+    static void RootProjectFolder()
+    {
+        string dataPath = Application.dataPath;
+
+        // Get rid of /Assets in the end of the path
+        // Example "E:/Unity Projects/ReferenceChecker/Assets" normal path
+        dataPath = dataPath.Substring(0, dataPath.LastIndexOf("/"));
+
+        System.Diagnostics.Process.Start("explorer.exe", dataPath.Replace('/', '\\'));
+    }
+
 }
