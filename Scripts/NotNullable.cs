9afc916259f8310750fd6a553f3d8725910b58cc
Marko Francuski <39244428+markofrancuski@users.noreply.github.com>
2021-08-08 20:39:17 +0200

    Add Custom Attribute script

---
+++ Scripts/NotNullable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// Custom Attribute. 
+/// Being used to check if the fields with this attribute have reference.
+/// </summary>
+[AttributeUsage(AttributeTargets.Field)]
+public class NotNullable : PropertyAttribute
+{
+
+}
